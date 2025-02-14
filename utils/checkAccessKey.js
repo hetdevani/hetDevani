f0f47eb809ae9ff418197c6252cdbca73a27d6f9
jaydip.sonani@cloudusinfotech.com <harshsuhagiya.cloudus22@gmail.com>
2025-02-14 09:46:53 +0530

    fix some issue

--- utils/checkAccessKey.js
+++ utils/checkAccessKey.js
@@ -1,19 +1,20 @@
 exports.checkAccess = async (req, res, next) => {
     try {
         const key = req.body.key || req.query.key || req.headers.key;
-        console.log('key :>> ', key);
+        console.log("Received Key:", key);
+        console.log("Expected Key from Env:", process.env.key); // Log env variable
         
-        if (key) {
-            if (key === process.env.key) { 
-                return next();
-            } else {
-                return res.status(401).json({ message: "Unauthorized access!" });
-            }
+        if (!process.env.key) {
+            return res.status(500).json({ message: "KEY is not set in environment variables!" });
+        }
+
+        if (key === process.env.key) { 
+            return next();
         } else {
             return res.status(401).json({ message: "Unauthorized access!" });
         }
     } catch (error) {
-        console.error('Error in checkAccess:', error);
+        console.error("Error in checkAccess:", error);
         return res.status(500).json({ message: "Internal Server Error" });
     }
 };
